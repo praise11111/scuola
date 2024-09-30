0b557b1926a8d7bbaa8f84273a1a72bc6a51e5b4
praise11111 <praise.osagiedeomosogie@galileiostiglia.edu.it>
2024-09-30 11:15:21 +0000

    OIUPO

---
+++ scuola/informatica/es1.c
@@ -0,0 +1,25 @@
+/*DATI DUE NUMERI A e B VERIFICARE
+    SE A è IL QUADRATO DI B     */
+
+    #include <stdio.h>
+    int main(){
+        float A=0;
+        float B=0;
+        float quadrato=0;
+
+        printf("inserisci il primo valore: ");
+        scanf("%f" , &A);
+        printf("inserisci il secondo valore: ");
+        scanf("%f" , %B);
+
+        quadrato = B*B;
+        printf("il doppio è: %f" , quadrato);
+
+        if(A== quadrato){
+            printf("il primo valore %f corrisponde al quadrato del secondo %f" , A,B);
+        }
+        else{
+            printf("il primo valore %f non corrisponde al quadrato del secondo %f" , A,B);
+        }
+
+ }
