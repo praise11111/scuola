828662697421b80c0df3da5e3372f2d24c1f6abe
praise11111 <praise.osagiedeomosogie@galileiostiglia.edu.it>
2024-10-01 09:34:40 +0200

    Update Es1.c

--- Es1.c
+++ Es1.c
@@ -4,7 +4,8 @@ int main(){
 
     printf("inserisci un numero: ");
     scanf("%d", &numero);
-    if(numero %3 == 0 && numero %5 == 0){
+    if(numero %3 == 0) 
+      (numero %5 == 0){
       printf("il numero %d è divisibile sia per 3 che per 5.\, numero");
     }else {
       printf("il numero %d non è divisibile sia per 3 che per 5.\, numero");
