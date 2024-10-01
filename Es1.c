34c31ff167d1d653dee2f634af8bd32dd13a66af
praise11111 <praise.osagiedeomosogie@galileiostiglia.edu.it>
2024-10-01 09:15:25 +0200

    Add files via upload

---
+++ Es1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int numero;
+
+    printf("inserisci un numero: ");
+    scanf("%d", &numero);
+    if(numero %3 == 0 && numero %5 == 0){
+      printf("il numero %d è divisibile sia per 3 che per 5.\, numero");
+    }else {
+      printf("il numero %d non è divisibile sia per 3 che per 5.\, numero");
+    }
+}
